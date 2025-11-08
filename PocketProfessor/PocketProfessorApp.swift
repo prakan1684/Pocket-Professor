a8596a8344dd55dfd91d9c4b6298b7d874509f8d
prakan1684 <p.kandi73@gmail.com>
2025-11-07 20:14:31 -0600

    Initial Commit

---
+++ PocketProfessor/PocketProfessorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketProfessorApp.swift
+//  PocketProfessor
+//
+//  Created by Pranav Kandikonda on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PocketProfessorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
